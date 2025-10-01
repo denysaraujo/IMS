e0e2b67edd8c23b97a76d5667d81dd9c8da1ce8c
denilson.araujo <denilson.araujo@codata.pb.gov.br>
2025-10-01 16:00:44 -0300

    Ajusta o pedding do add.component

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, SidebarComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, SidebarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

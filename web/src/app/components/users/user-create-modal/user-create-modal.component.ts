418d2b8d7348f5f62fc1b3adf5121ac2a60cafc5
denysaraujo <denilson.araujo.info@gmail.com>
2026-08-31 11:57:38 -0300

    Refatora para user-create

---
+++ web/src/app/components/users/user-create-modal/user-create-modal.component.ts
@@ -0,0 +1,44 @@
+import { Component, EventEmitter, Output, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { User } from '../../../services/user.service';
+
+@Component({
+  selector: 'app-create-user-modal',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './user-create-modal.component.html'
+})
+export class CreateUserModalComponent {
+  @Output() userCreated = new EventEmitter<User>();
+  @Output() cancel = new EventEmitter<void>();
+  @Input() user: User = {
+    nomeCompleto: '',
+    username: '',
+    password: '',
+    role: 'USER',
+    email: '',
+    telefone: '',
+    celular: '',
+    enabled: true
+  };
+
+  @Input() confirmPassword: string = '';
+
+  roles = [
+    { value: 'USER', label: 'Usuário' },
+    { value: 'MANAGER', label: 'Gerente' },
+    { value: 'ADMIN', label: 'Administrador' },
+    { value: 'SUPERVISOR', label: 'Supervisor' }
+  ];
+
+
+
+  onSubmit() {
+    this.userCreated.emit(this.user);
+  }
+
+  onCancel() {
+    this.cancel.emit();
+  }
+}
